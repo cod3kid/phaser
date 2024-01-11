791fb83c5cf4cc588c7dfe631578903499b9fa31
cod3kid <djsufail@gmail.com>
2024-01-11 21:57:25 +0530

    Tic Tac Toe: Board of nine squares

--- tic-tac-toe/src/scenes/Game.js
+++ tic-tac-toe/src/scenes/Game.js
@@ -12,6 +12,11 @@ export class Game extends Scene {
   }
 
   create() {
+    this.createTitleText();
+    this.createBoard();
+  }
+
+  createTitleText() {
     this.titleText = this.add
       .text(this.screenWidth / 2, 100, "Tic Tac Toe", {
         fontSize: "34px",
@@ -21,5 +26,27 @@ export class Game extends Scene {
       .setOrigin(0.5);
 
     this.titleText.setFontStyle("Bold");
+  }
+
+  createBoard() {
+    let x = 300;
+    let y = 200;
+    const squareSize = 128;
+
+    for (let i = 1; i <= 9; i++) {
+      this.add
+        .rectangle(
+          x + (i % 3) * (squareSize + 5),
+          y,
+          squareSize,
+          squareSize,
+          0x8accff
+        )
+        .setOrigin(0);
+
+      if (i % 3 === 0) {
+        y += squareSize + 5;
+      }
+    }
   }
 }
