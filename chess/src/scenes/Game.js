28ae7ce36bfb7a778ccb4fab51fde1c73e7ae0bc
cod3kid <djsufail@gmail.com>
2024-01-16 18:00:46 +0530

    Chess: pieces placed

--- chess/src/scenes/Game.js
+++ chess/src/scenes/Game.js
@@ -1,4 +1,5 @@
 import { Scene } from "phaser";
+import { pieces } from "../utils/constant";
 
 export class Game extends Scene {
   constructor() {
@@ -6,11 +7,15 @@ export class Game extends Scene {
   }
 
   create() {
+    this.createBoard();
+  }
+
+  createBoard() {
     let x = 270;
     let y = 150;
     const squareSize = 64;
 
-    for (let i = 0; i < 64; i++) {
+    pieces.forEach((piece, i) => {
       let squareColor;
       const row = Math.floor((63 - i) / 8) + 1;
 
@@ -20,7 +25,7 @@ export class Game extends Scene {
         squareColor = i % 2 === 0 ? 0xb15a29 : 0xebe2c2;
       }
 
-      const currentCell = this.add.rectangle(
+      const cell = this.add.rectangle(
         x,
         y,
         squareSize,
@@ -28,12 +33,20 @@ export class Game extends Scene {
         squareColor
       );
 
+      if (piece != "") {
+        const placedPiece = this.add
+          .image(cell.x, cell.y, piece)
+          .setScale(0.1)
+          .setName(piece)
+          .setDepth(3);
+      }
+
       x += squareSize + 5;
 
       if ((i + 1) % 8 === 0) {
         x = 270;
         y += squareSize + 5;
       }
-    }
+    });
   }
 }
--- chess/src/scenes/Preloader.js
+++ chess/src/scenes/Preloader.js
@@ -1,5 +1,12 @@
 import { Scene } from "phaser";
 
+import king from "../assets/king.png";
+import queen from "../assets/queen.png";
+import knight from "../assets/knight.png";
+import bishop from "../assets/bishop.png";
+import rook from "../assets/rook.png";
+import pawn from "../assets/pawn.png";
+
 export class Preloader extends Scene {
   constructor() {
     super("Preloader");
@@ -7,7 +14,14 @@ export class Preloader extends Scene {
 
   init() {}
 
-  preload() {}
+  preload() {
+    this.load.image("king", king);
+    this.load.image("queen", queen);
+    this.load.image("knight", knight);
+    this.load.image("rook", rook);
+    this.load.image("bishop", bishop);
+    this.load.image("pawn", pawn);
+  }
 
   create() {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
