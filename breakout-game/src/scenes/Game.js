a5cec2730bf0ce5a743f96d1f3f0a1df0e491ab6
cod3kid <djsufail@gmail.com>
2024-01-24 03:18:09 +0530

    Breakout Game: Game Screen

--- breakout-game/src/scenes/Game.js
+++ breakout-game/src/scenes/Game.js
@@ -1,9 +1,30 @@
 import { Scene } from "phaser";
+import { SECONDARY_COLOR } from "../utils/constant";
 
 export class Game extends Scene {
   constructor() {
     super("Game");
+
+    this.canvasWidth;
+    this.canvasHeight;
   }
 
-  create() {}
+  init() {
+    this.canvasWidth = this.scale.width;
+    this.canvasHeight = this.scale.height;
+  }
+
+  create() {
+    this.createGameScreen();
+  }
+
+  createGameScreen() {
+    this.add.rectangle(
+      this.canvasWidth / 2,
+      this.canvasHeight / 2,
+      this.canvasWidth - 100,
+      this.canvasHeight - 100,
+      SECONDARY_COLOR
+    );
+  }
 }
---
+++ breakout-game/src/utils/constant.js
@@ -0,0 +1,2 @@
+export const PRIMARY_COLOR = 0x210d30;
+export const SECONDARY_COLOR = 0xc59df5;
