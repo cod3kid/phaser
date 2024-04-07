f8f23c444901ad63c366a22954f39cbd36ca00ba
cod3kid <djsufail@gmail.com>
2024-04-08 03:39:36 +0530

    Snake Game - Create Board of 8x8

--- snake-game/src/scenes/Game.js
+++ snake-game/src/scenes/Game.js
@@ -3,7 +3,33 @@ import { Scene } from "phaser";
 export class Game extends Scene {
   constructor() {
     super("Game");
+
+    this.rows = 8;
+    this.cols = 8;
+    this.size = 40;
+    this.board = [];
   }
 
-  create() {}
+  create() {
+    this.createBoard();
+  }
+
+  createBoard() {
+    const baseXY = { x: 200, y: 200 };
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0x000000, 1);
+
+    for (let i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.board[i][j] = 1;
+        graphics.strokeRect(
+          baseXY.x + i * this.size,
+          baseXY.y + j * this.size,
+          this.size,
+          this.size
+        );
+      }
+    }
+  }
 }
