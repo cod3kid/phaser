b5dd9d53620eb10ab66e11e8bedf07d19acfd806
cod3kid <djsufail@gmail.com>
2024-04-09 19:13:42 +0530

    Create food

--- snake-game/src/scenes/Game.js
+++ snake-game/src/scenes/Game.js
@@ -8,6 +8,7 @@ export class Game extends Scene {
     this.cols = 8;
     this.size = 40;
     this.board = [];
+    this.food = { x: 0, y: 0 };
   }
 
   create() {
@@ -16,20 +17,45 @@ export class Game extends Scene {
 
   createBoard() {
     const baseXY = { x: 200, y: 200 };
-    const graphics = this.add.graphics();
-    graphics.lineStyle(2, 0x000000, 1);
+    this.gridGraphics = this.add.graphics();
+    this.gridGraphics.lineStyle(2, 0x000000, 1);
 
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = [];
       for (let j = 0; j < this.cols; j++) {
         this.board[i][j] = 1;
-        graphics.strokeRect(
-          baseXY.x + i * this.size,
-          baseXY.y + j * this.size,
-          this.size,
-          this.size
-        );
       }
+    }
+
+    this.board[this.food.x][this.food.y] = -1;
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (this.board[i][j] == 1) {
+          this.createSquare(baseXY, i, j, false);
+        } else if (this.board[i][j] == -1) {
+          this.createSquare(baseXY, i, j, true);
+        }
+      }
+    }
+  }
+
+  createSquare(baseXY, i, j, isFill) {
+    this.gridGraphics.strokeRect(
+      baseXY.x + i * this.size,
+      baseXY.y + j * this.size,
+      this.size,
+      this.size
+    );
+
+    if (isFill) {
+      this.gridGraphics.fillStyle(0xff0000);
+      this.gridGraphics.fillRect(
+        baseXY.x + i * this.size,
+        baseXY.y + j * this.size,
+        this.size,
+        this.size
+      );
     }
   }
 }
